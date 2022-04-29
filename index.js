ada4c7a8a739ff61fd551b3b606c7702d6634495
Afridi Azmain <Azmain951>
2022-04-29 16:32:59 +0600

    delete item api created

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
             const result = await fruitCollection.updateOne(filter, updatedFruit, options);
             res.send(result);
         });
+
+        app.delete('/fruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await fruitCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
